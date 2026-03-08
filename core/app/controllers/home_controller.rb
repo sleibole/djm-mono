1f28bb35875d5916cf16a758df8b8fbe952ba648
Sheldon Leibole <sheldonleibole@gmail.com>
2026-03-08 14:37:51 -0700

    Homepage as landing page for logged out visitors

--- core/app/controllers/home_controller.rb
+++ core/app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  layout :choose_layout
+
   def show
+  end
+
+  private
+
+  def choose_layout
+    logged_in? ? "application" : "landing"
   end
 end
